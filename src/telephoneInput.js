66ed63a5686f8aca876b26fd6b435eed73f8ee68
Franze Jr <franzejr@gmail.com>
2016-06-20 09:42:09 -0300

    #1 Add component

---
+++ src/telephoneInput.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import React, { Component, PropTypes } from 'react';
+
+export default class TelephoneInput extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <input type="text" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    
+  },
+});
